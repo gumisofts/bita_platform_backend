76bd7c7d5696cb86513e96cbbd9b9000c4ad75b5
Nathnael <nathnaelyirga@gmail.com>
2025-02-19 01:41:34 +0300

    file module added

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
+
+    # file urls
+    path("api/", include("file.urls")),
 ]
