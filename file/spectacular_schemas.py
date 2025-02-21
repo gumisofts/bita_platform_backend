6a88d0a6ff3a8349baeed8179f964d7a925e541f
Nathnael <nathnaelyirga@gmail.com>
2025-02-21 16:35:41 +0300

    [fix] comments cleaned

--- file/spectacular_schemas.py
+++ file/spectacular_schemas.py
@@ -2,26 +2,6 @@
 from django.core.files import File
 from drf_spectacular.utils import extend_schema, OpenApiTypes, OpenApiParameter, OpenApiResponse, OpenApiExample
 from .serializers import FileUploadSerializer
-#
-# file_upload_schema = extend_schema(
-#         request=FileUploadSerializer,
-#         responses={
-#             201: OpenApiResponse(response=FileUploadSerializer, description="File uploaded successfully"),
-#             400: OpenApiResponse(description="Invalid file or request"),
-#         },
-#         summary="Upload a file",
-#         description="Uploads a file and returns the content type of the uploaded file.",
-        # parameters=[
-        #     OpenApiParameter(
-        #         name="file",
-        #         type=OpenApiTypes.BYTE,
-        #         location=OpenApiParameter.HEADER,
-        #         required=True,
-        #         description="File to be uploaded"
-        #     )
-        # ]
-
-    # )
 
 
 file_upload_schema = extend_schema(
