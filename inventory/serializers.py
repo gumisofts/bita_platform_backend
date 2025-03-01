54d0849cca991ed9bc1c3040b35bc38171a28034
kaleb wondimu <kalebwondimu33@gmail.com>
2025-03-01 17:51:25 +0300

    working on restock

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,124 +1,13 @@
-# from rest_framework import serializers
+from rest_framework import serializers
+from .models import SuppliedItem,Supply
+class SuppliedItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SuppliedItem
+        fields = '__all__'
 
-# from .models import (
-#     Category,
-#     Item,
-#     ItemImage,
-#     Location,
-#     ReturnRecall,
-#     StockMovement,
-#     Store,
-#     Supply,
-#     SupplyReservation,
-# )
-# from .utils import upload_to_file_service, validate_image_file
+class SupplySerializer(serializers.ModelSerializer):
+    supplied_items = SuppliedItemSerializer(many=True, read_only=True)
 
-
-# class CategorySerializer(serializers.ModelSerializer):
-#     item_count = serializers.IntegerField(read_only=True)
-
-#     class Meta:
-#         model = Category
-#         fields = ["id", "name", "description", "item_count"]
-
-
-# class ItemSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Item
-#         fields = [
-#             "id",
-#             "name",
-#             "description",
-#             "category",
-#             "manufacturer",
-#             "barcode",
-#             "is_returnable",
-#             "notify_below",
-#             "isvisible",
-#         ]
-
-
-# class StoreSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Store
-#         fields = ["id", "name", "business_id", "location"]
-
-
-# class SupplySerializer(serializers.ModelSerializer):
-
-#     class Meta:
-#         model = Supply
-#         fields = [
-#             "id",
-#             "item",
-#             "quantity",
-#             "unit",
-#             "cost_price",
-#             "sale_price",
-#             "expiration_date",
-#             "batch_number",
-#             "man_date",
-#             "store",
-#             "supplier_id",
-#         ]
-
-
-# class LocationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Location
-#         fields = "__all__"
-
-
-# class StockMovementSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = StockMovement
-#         fields = ["id", "supply", "from_store", "to_store", "quantity", "reason"]
-
-
-# class ReturnRecallSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = ReturnRecall
-#         fields = ["id", "item", "quantity", "reason", "status"]
-
-
-# class ItemImageSerializer(serializers.ModelSerializer):
-#     file = serializers.FileField(write_only=True)
-
-#     class Meta:
-#         model = ItemImage
-#         fields = ["id", "item", "image_id", "file"]
-#         read_only_fields = ["image_id"]
-#         extra_kwargs = {
-#             "file": {"write_only": True},
-#         }
-
-#     def validate_file(self, value):
-#         validate_image_file(value)
-#         return value
-
-#     def create(self, validated_data):
-
-#         file = validated_data.pop("file")
-
-#         file_id = upload_to_file_service(file)
-
-#         item_image = ItemImage.objects.create(file_id=file_id, **validated_data)
-
-#         return item_image
-
-
-# class SupplyReservationSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = SupplyReservation
-#         fields = ["id", "supply", "quantity", "reserved_at", "status"]
-
-#     def validate(self, data):
-#         supply = data.get("supply")
-#         quantity = data.get("quantity")
-#         if supply and quantity and quantity > supply.quantity:
-#             raise serializers.ValidationError(
-#                 {
-#                     "quantity": "Reservation quantity cannot exceed available supply quantity."
-#                 }
-#             )
-#         return data
+    class Meta:
+        model = Supply
+        fields = '__all__'
--- inventory/urls.py
+++ inventory/urls.py
@@ -1,20 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from inventory.views import *
+from .views import SupplyViewSetV1, SuppliedItemViewSetV1
 
 router = DefaultRouter()
-# router.register("items", views.ItemViewSet, basename="items")
-# router.register("categories", views.CategoryViewSet, basename="categories")
-# router.register("supply", views.SupplyViewSet, basename="supplies")
-# router.register("store", views.StoreViewSet, basename="stores")
-# router.register("location", views.LocationViewSet, basename="locations")
-# router.register("stock-movement", views.StockMovementViewSet)
-# router.register("reservations", views.SupplyReservationViewSet, basename="reservations")
+router.register(r'supplies', SupplyViewSetV1
+                )
+router.register(r'supplied-items', SuppliedItemViewSetV1)
 
-# # items_router = routers.NestedDefaultRouter(router, "items", lookup="item")
-# # items_router.register("images", views.ItemImageViewSet, basename="item-images")
-
-
-# # URLConf
-urlpatterns = []
+urlpatterns = [
+    path('', include(router.urls)),
+]
