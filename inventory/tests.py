2a6554bd48f73ec0806378ce70134b4eefee0c38
abeni-al7 <abenezeralebachew3@gmail.com>
2025-03-01 20:45:19 +0300

    [isort] Sort dependencies

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,6 +1,7 @@
-from accounts.models import Business, Category
 from rest_framework import status
 from rest_framework.test import APITestCase
+
+from accounts.models import Business, Category
 
 from .models import Item
 
