b2deb222d863aef35d24881dadbb4ce451ee0e4a
abeni-al7 <abenezeralebachew3@gmail.com>
2025-03-10 16:37:54 +0300

    Fix isort

--- accounts/models.py
+++ accounts/models.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import RegexValidator
 from django.db import models
--- crm/models.py
+++ crm/models.py
@@ -1,4 +1,5 @@
 from uuid import uuid4
+
 from django.contrib.auth import get_user_model
 from django.core.validators import RegexValidator
 from django.db import models
