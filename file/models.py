74f15430760714e0ab784467fc2f914ee56e004d
Nathnael <nathnaelyirga@gmail.com>
2025-03-14 00:24:25 +0300

    - saved_as field used as pk - loose imports fixed

--- file/models.py
+++ file/models.py
@@ -1,44 +1,22 @@
+import io
 import logging
 import os
-from io import BytesIO
 from uuid import uuid4
 
+from django.core.files.base import ContentFile
 from django.db import models
-from django.utils.timezone import now
+from PIL import Image
 from storages.backends.s3boto3 import S3Boto3Storage
 
 logger = logging.getLogger(__name__)
 
 
-def file_upload_to(instance, filename):
-    logger.info(f"Processing field: for file: {filename}")
-    file_extension = filename.split(".")[-1].lower()
-    file_folder = {
-        "png": "images/",
-        "jpg": "images/",
-        "jpeg": "images/",
-        "gif": "images/",
-        "mp3": "audio/",
-        "mp4": "video/",
-        "pdf": "documents/",
-    }.get(file_extension, "others/")
-    # Generate a unique filename for each version of the image
-    if hasattr(instance, "optimized_image") and instance.optimized_image:
-        return os.path.join(
-            file_folder, f"{instance.stored_as}_optimized.{file_extension}"
-        )
-    elif hasattr(instance, "thumbnail") and instance.thumbnail:
-        return os.path.join(
-            file_folder, f"{instance.stored_as}_thumbnail.{file_extension}"
-        )
-    else:
-        return os.path.join(file_folder, f"xcx{instance.stored_as}.{file_extension}")
-
-
 class FileModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    stored_as = models.CharField(
+        primary_key=True, max_length=255, unique=True, blank=True, default=uuid4
+    )
+
     name = models.CharField(max_length=255)
-    stored_as = models.CharField(max_length=255, unique=True, blank=True, null=True)
 
     # Store the original file
     file = models.FileField(storage=S3Boto3Storage(), null=True, blank=True)
@@ -55,24 +33,21 @@ class FileModel(models.Model):
     retrieval_count = models.PositiveIntegerField(default=0)
     file_extension = models.CharField(max_length=10, blank=True)
 
-    # def __str__(self):
-    #     return self.name
-
     def __str__(self):
         return self.name
 
     def save(self, *args, **kwargs):
-        if not self.stored_as:
-            self.stored_as = uuid4().hex  # Generate a unique ID
-            # self.file_extension = os.path.splitext(self.file.name)[1].lower()  # Extract file extension
-            self.file_extension = os.path.splitext(self.file.name)[
-                1
-            ].lower()  # Extract file extension
+
         # Set file size before saving
         if self.file:
+            # get extension
+            self.file_extension = os.path.splitext(self.file.name)[1].lower()
+            # build filename
             original_filename = f"{self.stored_as}{self.file_extension}"
-            self.file.name = original_filename  # Set the new filename
-            self.file_size = self.file.size  # Set file size
+            # set file name and size
+            self.file.name = original_filename
+            self.file_size = self.file.size
+            print(self.file.name)
 
         super().save(*args, **kwargs)  # Save the original file first!
 
@@ -84,16 +59,6 @@ class FileModel(models.Model):
             )  # Save only these fields
 
     def generate_image_versions(self):
-        import io
-
-        import requests
-        from django.core.files.base import ContentFile
-        from PIL import Image
-
-        # Read the image from S3
-        # response = requests.get(self.file.url, stream=True)
-        # response.raise_for_status()
-        # img = Image.open(BytesIO(response.content))
 
         with self.file.open("rb") as f:
             img = Image.open(f)
@@ -131,7 +96,7 @@ class FileModel(models.Model):
         thumbnail_io = io.BytesIO()
         thumbnail.save(thumbnail_io, format="JPEG", quality=75)
         thumbnail_file = ContentFile(thumbnail_io.getvalue(), name=thumbnail_filename)
-
+        print(thumbnail_filename, optimized_filename)
         # Assign to fields and save
         self.optimized_image.save(optimized_filename, optimized_file, save=False)
         self.thumbnail.save(thumbnail_filename, thumbnail_file, save=False)
