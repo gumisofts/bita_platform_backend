6a93e39c4ead6500c389ae654f1e0ee990d93612
Murad <nuradhussen082@gmail.com>
2025-04-15 16:59:29 +0300

    status code fix

--- accounts/views.py
+++ accounts/views.py
@@ -91,6 +91,13 @@ class LoginViewset(CreateModelMixin, GenericViewSet):
 class LoginWithGoogleViewset(CreateModelMixin, GenericViewSet):
     serializer_class = LoginWithGoogleIdTokenSerializer
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
+
 
 class RefreshLoginViewset(CreateModelMixin, GenericViewSet):
     serializer_class = RefreshLoginSerializer
