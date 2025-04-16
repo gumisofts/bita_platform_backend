142fa45d2b61f896905323e35172e171bd1e70c3
Murad <nuradhussen082@gmail.com>
2025-04-16 16:02:14 +0300

    fix status code

--- accounts/views.py
+++ accounts/views.py
@@ -170,3 +170,10 @@ class BranchViewSet(viewsets.ModelViewSet):
 
 class ConfirmVerificationCodeViewset(CreateModelMixin, GenericViewSet):
     serializer_class = ConfirmVerificationCodeSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK, headers=headers)
