98da20077ee865a9605b9884d8c9a1762a0d611f
Murad <nuradhussen082@gmail.com>
2025-10-09 01:49:37 +0300

    formatted

--- orders/signals.py
+++ orders/signals.py
@@ -1,5 +1,5 @@
 # Signal for creating a transaction when an order is completed
-from django.db.models.signals import post_save, Signal
+from django.db.models.signals import Signal, post_save
 from django.dispatch import receiver
 
 from orders.models import Order
