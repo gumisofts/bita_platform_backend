dbc0e70bf5ce170952bf2126d4f1170cab29a7a6
Murad <nuradhussen082@gmail.com>
2025-10-08 09:14:01 +0300

    patch fixes

--- inventories/views.py
+++ inventories/views.py
@@ -214,7 +214,9 @@ class ItemVariantViewset(ModelViewSet):
         )
 
     def update(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return Response(
