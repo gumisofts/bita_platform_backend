b3b918801a673fa4975f77f95433b2b8bcc560e4
Murad <nuradhussen082@gmail.com>
2025-10-11 17:26:53 +0300

    variant sold

--- orders/signals.py
+++ orders/signals.py
@@ -2,6 +2,14 @@
 from django.db.models.signals import Signal, post_save
 from django.dispatch import receiver
 
+from inventories.signals import on_item_variant_sold
 from orders.models import Order
 
 order_completed = Signal()
+
+
+@receiver(order_completed)
+def on_order_completed(sender, instance, **kwargs):
+    for item in instance.items.all():
+        item.variant.quantity -= item.quantity
+        item.variant.save()
