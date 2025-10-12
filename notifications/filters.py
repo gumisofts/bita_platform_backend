9fcbffc92db4d39049141005e77534a12eae003f
Murad <nuradhussen082@gmail.com>
2025-10-12 08:04:48 +0300

    format

--- notifications/filters.py
+++ notifications/filters.py
@@ -1,10 +1,11 @@
-from django_filters import FilterSet, DateFilter
+from django_filters import DateFilter, FilterSet
 
 from .models import Notification
 
 
 class NotificationFilter(FilterSet):
     timestamp = DateFilter(field_name="created_at", lookup_expr="gte")
+
     class Meta:
         model = Notification
-        fields = ["notification_type", "delivery_method", "created_at",'timestamp']
+        fields = ["notification_type", "delivery_method", "created_at", "timestamp"]
