9b291c6be749cc18345349d69b44439561da5d73
Bisrat Dereje <bdereje996@gmail.com>
2025-10-28 15:12:00 +0300

    fix: remove override create fun

--- administration/views.py
+++ administration/views.py
@@ -30,23 +30,7 @@ class WaitlistViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Waitlist.objects.all()
     serializer_class = WaitlistSerializer
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        instance = serializer.save()
-        headers = self.get_success_headers(serializer.data)
-        response_data = getattr(serializer, "_response_data", serializer.data)
-        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
-
 
 class ContactViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        instance = serializer.save()
-        headers = self.get_success_headers(serializer.data)
-        response_data = getattr(serializer, "_response_data", serializer.data)
-        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
