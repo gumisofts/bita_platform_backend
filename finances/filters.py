333cd059b6f203d9b96010090d2586d78a1f4314
Murad <nuradhussen082@gmail.com>
2026-05-20 23:00:43 +0300

    bug fix

--- finances/filters.py
+++ finances/filters.py
@@ -6,11 +6,12 @@ from finances.models import BusinessPaymentMethod
 
 
 class BusinessPaymentMethodFilter(FilterSet):
-    branch = django_filters.NumberFilter(method="filter_branch")
+    branch = django_filters.UUIDFilter(method="filter_branch")
 
     class Meta:
         model = BusinessPaymentMethod
         fields = ["business", "branch", "payment"]
 
     def filter_branch(self, queryset, name, value):
+        print(name, value)
         return queryset.filter(models.Q(branch=value) | models.Q(branch__isnull=True))
