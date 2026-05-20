863605f11ccfaf5a8e144657ef4d6612d76e7a0e
Murad <nuradhussen082@gmail.com>
2026-05-20 21:12:29 +0300

    payment methods

--- finances/filters.py
+++ finances/filters.py
@@ -1,9 +1,16 @@
+import django_filters
+from django.db import models
 from django_filters import FilterSet
 
 from finances.models import BusinessPaymentMethod
 
 
 class BusinessPaymentMethodFilter(FilterSet):
+    branch = django_filters.NumberFilter(method="filter_branch")
+
     class Meta:
         model = BusinessPaymentMethod
         fields = ["business", "branch", "payment"]
+
+    def filter_branch(self, queryset, name, value):
+        return queryset.filter(models.Q(branch=value) | models.Q(branch__isnull=True))
